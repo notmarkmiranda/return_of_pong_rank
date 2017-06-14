d1f084ca0d8270a6eefa9fba9eac20800a8ef12d
Mark Miranda <notmarkmiranda@gmail.com>
2017-06-14 14:14:20 -0600

    initial branch commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,42 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+    registrations: 'devise_customizations/registrations',
+  }
+
+  resources :users do
+    member do
+      get 'analytics_alias'
+    end
+  end
+
+  namespace :admin do
+    resources :users do
+      member do
+        get 'impersonate'
+      end
+
+      collection do
+        get 'stop_impersonating'
+      end
+    end
+  end
+
+  authenticated :user do
+    # root to: 'dashboard#show', as: :authenticated_root
+    root to: 'high_voltage/pages#show', id: 'welcome', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    get 'sign-in',  to: 'devise/sessions#new'
+    get 'sign-out', to: 'devise/sessions#destroy'
+  end
+  root 'high_voltage/pages#show', id: 'welcome'
+
+  # API-specific routes
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users, except: [:new, :edit]
+    end
+  end
 end
