2acb0f904079f99223a97be34608b5e47f7338f2
Mark Miranda <notmarkmiranda@gmail.com>
2017-06-16 21:07:02 -0600

    devise wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def after_sign_in_path(users)
+    grid_index_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:authid, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_in) { |i| u.permit(:authid, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:authid, :email, :password, :password_confirmation, :current_password) }
+  end
 end
